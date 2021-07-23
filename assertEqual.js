e677a6fdd4ccc1a07e04f4df1fb8768e996e205a
Zubaer <bijon7@yahoo.com>
2021-07-22 23:32:15 -0700

    Adding assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// Program takes two arguments and compares if they are equal in value
+const assertEqual = function(actual, expected) {
+//Prints out corresponding messages based on whether the arguments
+//are equal or not.
+  if (actual=== expected) {
+    console.log ("✅✅✅Assertion Passed: " + actual + "===" + " expected")
+  }
+  else {
+    console.log ("❌❌❌Assertion Failed: " + actual +  "!==" + " expected")
+
+}
+  }
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
