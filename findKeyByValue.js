6b6dd27d10d0b0918db8ca746a28e44b532d6679
Zubaer <bijon7@yahoo.com>
2021-08-07 17:54:04 -0700

    first version

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,7 @@
+//This program takes in an object and a value. It should scan the object
+//and return the first key which contains the given value. If no key with that
+//given value is found, then it should return undefined.
+
 findKeyByValue = function (obj, value) { 
   
   for (let element in obj) {
