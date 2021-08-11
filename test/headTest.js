bc1cb0c80579cc86ee0a3195a96232cf84285428
Zubaer <bijon7@yahoo.com>
2021-08-11 15:10:12 -0700

    first version

---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+const headOfArray = require('../head.js');
+// Verifies whether the first element does equal the input using numbers, strings
+//single element array as well as an empty array.
+assertEqual(headOfArray([2, 3, 5]), 2);
+assertEqual(headOfArray(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(headOfArray([2]), 2);
+assertEqual(headOfArray([]), 2);
