1500c9cd961f43f2dcd32ce0349fc35bc3861ec8
Zubaer <bijon7@yahoo.com>
2021-08-13 15:33:03 -0700

    testing .ignore files

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,14 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const headOfArray = require('../head.js');
 // Verifies whether the first element does equal the input using numbers, strings
 //single element array as well as an empty array.
-assertEqual(headOfArray([2, 3, 5]), 2);
-assertEqual(headOfArray(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(headOfArray([2]), 2);
-assertEqual(headOfArray([]), 3);
+// assertEqual(headOfArray([2, 3, 5]), 2);
+// assertEqual(headOfArray(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(headOfArray([2]), 2);
+// assertEqual(headOfArray([]), 3);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
